6151ef836d0565ad52476a67c13c68264336c7f6
“Skychutt” <phycl@263.net>
2026-08-01 11:54:45 +0800

    补充了线性回归基础算法的讲解

--- 机器学习/Supervised_learning_algorithms/Linear_model/Linear_regression.py
+++ 机器学习/Supervised_learning_algorithms/Linear_model/Linear_regression.py
@@ -10,8 +10,14 @@ lr.fit(X_train, y_train)
 print("斜率{}".format(lr.coef_))
 print("截距{}".format(lr.intercept_))
 
+#这种情况是欠拟合 训练集和测试机评分接近且都比较低
 print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
 print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+
+#这种情况是过拟合，训练集准但是测试机不够准，因为506样本，105特征，特征数过多导致拟合程度太大
+X,y  = mglearn.datasets.make_wave(n_samples=60)
+
 
 
 
